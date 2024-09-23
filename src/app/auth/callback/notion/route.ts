92251e94832b6c0226b87f4e79ffa0bd19d13329
Jaehyeon <xhwogusxh@gmail.com>
2024-09-23 20:48:31 +0900

    feature: notion/api/database/route.ts 수정

--- src/app/auth/callback/notion/route.ts
+++ src/app/auth/callback/notion/route.ts
@@ -23,89 +23,94 @@ export async function GET(request: Request) {
     const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString(
       "base64"
     );
+    try {
+      const response = await fetch("https://api.notion.com/v1/oauth/token", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Basic ${encoded}`,
+        },
+        body: JSON.stringify({
+          grant_type: "authorization_code",
+          code: `${code}`,
+          redirect_uri: redirectUri,
+        }),
+      });
 
-    const response = await fetch("https://api.notion.com/v1/oauth/token", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Basic ${encoded}`,
-      },
-      body: JSON.stringify({
-        grant_type: "authorization_code",
-        code: `${code}`,
-        redirect_uri: redirectUri,
-      }),
-    });
+      const responseJson = await response.json();
 
-    const responseJson = await response.json();
+      // if (!error) {
+      //   return NextResponse.redirect(`${origin}${next}`);
+      // }
 
-    // if (!error) {
-    //   return NextResponse.redirect(`${origin}${next}`);
-    // }
+      if (responseJson.owner.type != "user") {
+        // 개인유저가 아니라면 법인이나 이런 걸 껀데, 이거 처리해줘야함.
+        return NextResponse.redirect(
+          `${origin}/notion-connect?success=false&message=onlyPersonalUser`
+        );
+      }
 
-    if (responseJson.owner.type != "user") {
-      // 개인유저가 아니라면 법인이나 이런 걸 껀데, 이거 처리해줘야함.
-      return NextResponse.redirect(
-        `${origin}/notion-connect?success=false&message=onlyPersonalUser`
-      );
-    }
+      const supabase = createClient();
+      const user = await supabase.auth.getUser();
 
-    const supabase = createClient();
-    const user = await supabase.auth.getUser();
+      if (!user.data.user) {
+        return NextResponse.redirect(
+          `${origin}/please-login?message=${LOGIN_AGAIN}`
+        );
+      }
 
-    if (!user.data.user) {
-      return NextResponse.redirect(
-        `${origin}/please-login?message=${LOGIN_AGAIN}`
-      );
-    }
-
-    const notionInfoByWorkspaceId = await supabase
-      .from("notion_info")
-      .select("id")
-      .eq("user_id", user.data.user.id)
-      .eq("workspace_id", responseJson.workspace_id);
-
-    // 기존에 workspaceId가 겹치는 게 없다면 notionInfo를 저장하고, 있다면 update해준다!!!
-    if (
-      notionInfoByWorkspaceId.data &&
-      notionInfoByWorkspaceId.data.length > 0
-    ) {
-      // 기존에 workspaceId가 겹치는 notionInfo가 있다면 업데이트
-      const { data, error } = await supabase
+      const notionInfoByWorkspaceId = await supabase
         .from("notion_info")
-        .update({
-          access_token: responseJson.access_token,
-          token_type: responseJson.token_type,
-          bot_id: responseJson.bot_id,
-          workspace_name: responseJson.workspace_name,
-        })
+        .select("id")
         .eq("user_id", user.data.user.id)
         .eq("workspace_id", responseJson.workspace_id);
 
-      if (error) {
-        // error handling
-        return NextResponse.redirect(
-          `${origin}/notion-connect?success=false&message=server-error`
-        );
-      }
-    } else {
-      // 기존에 workspaceIdrk 겹치는 notionInfo가 없다면 인서트. 혹시모르니 걍 upsert로 해줌.
-      const { data, error } = await supabase.from("notion_info").upsert({
-        access_token: responseJson.access_token,
-        token_type: responseJson.token_type,
-        bot_id: responseJson.bot_id,
-        workspace_id: responseJson.workspace_id,
-        workspace_name: responseJson.workspace_name,
-        user_id: user.data.user?.id,
-      });
+      // 기존에 workspaceId가 겹치는 게 없다면 notionInfo를 저장하고, 있다면 update해준다!!!
+      if (
+        notionInfoByWorkspaceId.data &&
+        notionInfoByWorkspaceId.data.length > 0
+      ) {
+        // 기존에 workspaceId가 겹치는 notionInfo가 있다면 업데이트
+        const { data, error } = await supabase
+          .from("notion_info")
+          .update({
+            access_token: responseJson.access_token,
+            token_type: responseJson.token_type,
+            bot_id: responseJson.bot_id,
+            workspace_name: responseJson.workspace_name,
+          })
+          .eq("user_id", user.data.user.id)
+          .eq("workspace_id", responseJson.workspace_id);
 
-      if (error) {
-        // error handling
-        return NextResponse.redirect(
-          `${origin}/notion-connect?success=false&message=server-error`
-        );
+        if (error) {
+          // error handling
+          return NextResponse.redirect(
+            `${origin}/notion-connect?success=false&message=server-error`
+          );
+        }
+      } else {
+        // 기존에 workspaceIdrk 겹치는 notionInfo가 없다면 인서트. 혹시모르니 걍 upsert로 해줌.
+        const { data, error } = await supabase.from("notion_info").upsert({
+          access_token: responseJson.access_token,
+          token_type: responseJson.token_type,
+          bot_id: responseJson.bot_id,
+          workspace_id: responseJson.workspace_id,
+          workspace_name: responseJson.workspace_name,
+          user_id: user.data.user?.id,
+        });
+
+        if (error) {
+          // error handling
+          return NextResponse.redirect(
+            `${origin}/notion-connect?success=false&message=server-error`
+          );
+        }
       }
+    } catch (err) {
+      return NextResponse.redirect(
+        `${origin}/notion-connect?success=false&message=server-error`
+      );
     }
 
     return NextResponse.redirect(`${origin}/notion-connect?success=true`);
